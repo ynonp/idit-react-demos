28b8d8096a74fb13d3e32b146d3e10da78ab5d3c
ynonp <ynonperek@gmail.com>
2019-11-06 12:53:23 +0200

    solved days of the week

--- demos/day2/src/days_of_the_week.js
+++ demos/day2/src/days_of_the_week.js
@@ -2,23 +2,51 @@ import React from 'react';
 import { useState } from 'react';
 import _ from 'lodash';
 
+const days_of_the_week = [
+  'Monday', 'Tuesday', 'Wednesday',
+  'Thursday', 'Friday', 'Saturday', 'Sunday',
+];
+
 export default function() {
-  const [days, setDays] = useState(
-     ['Monday', 'Tuesday', 'Wednesday',
-      'Thursday', 'Friday', 'Saturday', 'Sunday']);
+  const [days, setDays] = useState(days_of_the_week);
+  const [selectedDays, setSelectedDays] = useState(new Set([]));  
+
+  function reset() {
+    setDays(days_of_the_week);
+    setSelectedDays(new Set([]));
+  }
 
   function shuffle() {
     setDays(_.shuffle(days));
   }
 
+  function handleDelete() {
+    setDays(days.filter((day) => !selectedDays.has(day)));
+  }
+
+  function handleChange(e, day) {
+    if (e.target.checked) {
+      selectedDays.add(day);
+    } else {
+      selectedDays.delete(day);
+    }
+    setSelectedDays(selectedDays);
+  }
+
+  // npx webpack-dev-server -d
   return (
     <div>
       <button onClick={shuffle}>Shuffle</button>
-      <button>Delete</button>
-      <button>Reset</button>
+      <button onClick={handleDelete}>Delete</button>
+      <button onClick={reset}>Reset</button>
       <ul>        
         {days.map((day) => (
-          <li key={day}><input type="checkbox" />{day}</li>
+          <li key={day}>
+            <input
+              type="checkbox"
+              checked={selectedDays.has(day)}
+              onChange={(e) => handleChange(e, day)}
+          />{day}</li>
         ))}
       </ul>
     </div>
