30839e1c7eec3c3ff14a95de73b512c920f72d60
ynonp <ynonperek@gmail.com>
2019-11-11 11:48:32 +0200

    solved lab

--- demos/day3/src/main.js
+++ demos/day3/src/main.js
@@ -5,48 +5,62 @@ import _ from 'lodash';
 import $ from 'jquery';
 
 const App = () => {
-  const [id, setId] = useState(_.random(1, 10));
-  const [name, setName] = useState("Loading, please wait...");
+  const [id, setId] = useState(1);
+  const [character, setCharacter] = useState({});
+  const [filmId, setFilmId] = useState(1);
+  const [film, setFilm] = useState({});
 
   useEffect(function() {
+    setFilm({});
+    const req = $.getJSON(`https://swapi.co/api/films/${filmId}/`, function(data) {
+      setFilm(data);
+    });
+    return function cancel() {
+      req.abort();
+    }
+
+  }, [filmId]);
+
+  useEffect(function() {
+    setCharacter({});
     const req = $.getJSON(`https://swapi.co/api/people/${id}/`, function(data) {
-      setName(data.name);
+      setCharacter(data);
     });
     return function cancel() {
       req.abort();
     }
   }, [id]);
 
-  function shuffle() {
-    setId(_.random(1, 10));
-    setName('[Loading, please wait...]');
-  }
+  const characterNameText = (character.name == null ?
+    '[Loading please wait]' :
+    character.name);
+
+  const filmNameText = (film.title == null ?
+    '[Loading please wait]' :
+    film.title);
+
+  const filmIds = (character.films != null ?
+    character.films.map(f => f.match(/(\d+)\/$/)[1]) :
+    _.range(1, 8));
 
   return (
     <div>
       <h1>ID: {id}</h1>
       Character ID:
-      <select>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
+      <select onChange={(e) => setId(e.target.value)}>
+        {_.range(1, 11).map(id => (
+          <option value={id}>{id}</option>
+        ))}
       </select>
-      Film ID:
-      <select>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
-        <option>1</option>
+      Film ID :
+      <select onChange={(e) => setFilmId(e.target.value)}>
+        {filmIds.map(id => (
+          <option value={id}>{id}</option>
+        ))}
       </select>
-      <h1>Name: {name}</h1>
-      <button onClick={shuffle}>Shuffle</button>
+      <hr />
+      <h2>Character Name: {characterNameText}</h2>
+      <h2>Film Name: {filmNameText}</h2>
     </div>
   )
 };
